e9b96df1bcfd6db843e01985d8725deb0b0011ac
Phạm Văn Nguyên <npv2k1@gmail.com>
2024-03-10 16:16:28 +0700

    Update i18n configuration and edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "vi",
+    locales: ["vi", "en"],
   },
 
   presets: [
@@ -39,6 +39,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           path: "docs",
+          routeBasePath: "/",
           includeCurrentVersion: true,
         },
 
@@ -46,8 +47,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/pnstack/blog/edit/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
